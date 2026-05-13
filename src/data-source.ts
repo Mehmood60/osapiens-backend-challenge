1ad3519e023b73c576ccbb2e5945fa90bb0c04b1
Mehmood ul Hassan <m.ulhassan@mail.vetcontrol.team>
2026-05-13 20:13:01 +0200

    fix: preserve workflow data across server restarts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,12 @@
-import { DataSource } from 'typeorm';
-import { Task } from './models/Task';
-import {Result} from "./models/Result";
-import {Workflow} from "./models/Workflow";
+import { DataSource } from "typeorm";
+import { Task } from "./models/Task";
+import { Result } from "./models/Result";
+import { Workflow } from "./models/Workflow";
 
 export const AppDataSource = new DataSource({
-    type: 'sqlite',
-    database: 'data/database.sqlite',
-    dropSchema: true,
-    entities: [Task, Result, Workflow],
-    synchronize: true,
-    logging: false,
+  type: "sqlite",
+  database: "data/database.sqlite",
+  entities: [Task, Result, Workflow],
+  synchronize: true,
+  logging: false,
 });
